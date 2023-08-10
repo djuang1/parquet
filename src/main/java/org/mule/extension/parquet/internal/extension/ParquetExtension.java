47914f0513b0c2ed0ee8ca8c7f6961adddda32f2
Dejim Juang <dejimj@gmail.com>
2023-08-10 09:53:07 -0500

    Sonar Fixes

--- src/main/java/org/mule/extension/parquet/internal/extension/ParquetExtension.java
+++ src/main/java/org/mule/extension/parquet/internal/extension/ParquetExtension.java
@@ -1,19 +1,19 @@
-package org.mule.extension.parquet.internal;
+package org.mule.extension.parquet.internal.extension;
 
 import static org.mule.runtime.api.meta.Category.CERTIFIED;
 
+import org.mule.extension.parquet.internal.ParquetConfiguration;
 import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
 import org.mule.runtime.extension.api.annotation.license.RequiresEnterpriseLicense;
-
 
 /**
  * This is the main class of an extension, is the entry point from which configurations, connection providers, operations
  * and sources are going to be declared.
  */
 @Xml(prefix = "parquet")
-@Extension(name = "Parquet Connector - Mule 4", category = CERTIFIED, vendor = "Dejim Juang")
+@Extension(name = "Parquet Connector", category = CERTIFIED, vendor = "Dejim Juang")
 @RequiresEnterpriseLicense(allowEvaluationLicense = true)
 @Configurations(ParquetConfiguration.class)
 public class ParquetExtension {
